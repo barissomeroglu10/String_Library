5de93f54d554a592584482f14e2524182c486a8c
Barış Someroğlu <210260025@firat.edu.tr>
2024-06-26 23:38:47 +0300

    Update String.cpp

--- String/String.cpp
+++ String/String.cpp
@@ -10,20 +10,19 @@ Date = 19.06.2024 - 06.00 pm
 
 using namespace std;
 
-int main()
-{
+int main() {
   system("color E9");
 
-  // Example. shows string functions under string library
+  Example. shows string functions under string library
 
-   /* 
-   1. DEFINITION and INITIAL VALUE ASSIGNMENT
-    1a. DEFINITION
-    1b. USEING " "
-    1c. READING DATA WITH CONSOLE
-  */
+  /*
+  1. DEFINITION and INITIAL VALUE ASSIGNMENT
+   1a. DEFINITION
+   1b. USEING " "
+   1c. READING DATA WITH CONSOLE
+ */
 
-  /* 
+  
   string Example = "First String Definition.";
 
   cout << Example << endl;
@@ -36,9 +35,7 @@ int main()
   cin >> Example;
 
   cout << endl << Example << endl;
-  */
-
-
+  
 
   /*
   2. STRIING MEMORY FUNCTIONS
@@ -50,7 +47,7 @@ int main()
     2f. empty
   */
 
-  /*
+  
   string Example2 = "Learning String's Size.";
 
   cout << Example2 << endl << endl;
@@ -66,8 +63,8 @@ int main()
 
   cout << Example2.size() << endl << Example2 << endl;
 
-  // capacity function shows capactiy of string which gave by compiler 
-  // capacity can be 15, 31, 47, 63, 79 ..... 16n -1 
+  // capacity function shows capactiy of string which gave by compiler
+  // capacity can be 15, 31, 47, 63, 79 ..... 16n -1
   cout << "\nNow learning capacity!" << endl;
 
   cout << Example2.capacity() << endl;
@@ -77,7 +74,8 @@ int main()
 
   Example2.reserve(64);
 
-  cout << "Capacity of String After Reservation = " << Example2.capacity() << endl;
+  cout << "Capacity of String After Reservation = " << Example2.capacity() <<
+  endl;
 
   // clear function deletes string's ingredient
   // after .clear() string's size will be 0
@@ -87,7 +85,8 @@ int main()
 
   Example2.clear();
 
-  cout << "String content after clear = " << Example2 << Example2.length() << endl;
+  cout << "String content after clear = " << Example2 << Example2.length() <<
+  endl;
 
   // empty function checks string is empty or not empty
   // if it's empty ---> shows 1
@@ -98,9 +97,6 @@ int main()
 
   Example2 = "Now string not empty";
   cout << Example2 << "\n" << Example2.empty() << endl;
-  */
-
-
 
   /*
   3. ACCESS METHODS
@@ -108,7 +104,6 @@ int main()
     3b. [] OPERATOR
   */
 
-  /*
   string Example3 = "Leaning String Access Methods";
 
   // at function
@@ -118,11 +113,10 @@ int main()
   // [] operator
   cout << "\nFinding string character with using [] operator" << endl;
   cout << "Character at 2. index = " << Example3.at(2) << endl;
-  */
 
   /* 4.TEXT EDITING FUNCTIONS
-    - + operator 
-    - += operator 
+    - + operator
+    - += operator
     - append
     - push_back
     - assign
@@ -133,7 +127,7 @@ int main()
     - substr
   */
 
-   /* string Example4, Example5;
+  string Example4, Example5;
 
   cout << "Please Enter 1. String = ";
   cin >> Example4;
@@ -238,7 +232,8 @@ int main()
   cout << "Example5 = " << Example5 << endl;
 
   // replace = erase + insert
-  // replace firstly makes erase any position of a string then adds a string to any position of a string
+  // replace firstly makes erase any position of a string then adds a string to
+  // any position of a string
 
   Example4.replace(3, 3, Example5, 3, 3);
   Example5.replace(3, 3, Example4, 0, 3);
@@ -273,16 +268,103 @@ int main()
   Example5 = Example4.substr(0, 3);
 
   cout << "\nExample5 = Example4.substr(0, 3) ---> " << Example5 << endl;
- */
-
 
   /* 5.OTHER PROCCESSES
-    - c_str 
-    - data 
+    - c_str
+    - data
     - find
     - compare
     - Using spacial characters
   */
 
+  cout << "--------------------------------------------------";
+
+  Example4 = "String";
+  Example5 = "Adding";
+
+  cout << "\nExample4 = " << Example4 << endl;
+  cout << "Example5 = " << Example5 << endl;
+
+  // c_str converts string to char array (converts to c string)
+  Example4.c_str();
+  Example5.c_str();
+
+  cout << "\nExample4 after Example4.c_str() ---> " << Example4 << endl;
+
+  cout << "Example5 after Example5.c_str() ---> " << Example5 << endl;
+
+  // data converts string to char array (converts to c string)
+  Example4.data();
+  Example5.data();
+
+  cout << "\nExample4 after Example4.data() ---> " << Example4 << endl;
+  cout << "Example5 after Example5.data() ---> " << Example5 << endl;
+
+  cout << "--------------------------------------------------";
+
+  Example4 = "String Adding";
+  Example5 = "Adding";
+
+  cout << "\nExample4 = " << Example4 << endl;
+  cout << "Example5 = " << Example5 << endl;
+
+  // find finds a string in another string
+  cout << "\nExample4.find(Example5) ---> " << Example4.find(Example5) << endl;
+
+  cout << "\nExample5.find(Example4, 7, 6) --->" << Example5.find(Example4, 7)
+       << endl;
+
+  string Example6 = "Try";
+
+  cout << "\nExample4.find(Example6) ---> " << Example4.find(Example6) << endl;
+
+  cout << "--------------------------------------------------";
+
+  Example4 = "String comparing";
+  Example5 = "String";
+
+  cout << "\nExample4 = " << Example4 << endl;
+  cout << "Example5 = " << Example5 << endl;
+
+  // compare compares two strings
+  cout << "\nExample4.compare(Example5) --->" << Example4.compare(Example5) << endl;
+
+  cout << "\nExample4.compare(0, 6, Example5) ---> " << Example4.compare(0, 6, Example5) << endl;
+
+  cout << "\nExample4.compare(0, 2, Example5, 0, 3) ---> " << Example4.compare(0, 2, Example5, 0, 3) << endl;
+
+  /*
+  
+  Using spacial characters
+
+  \n or \r = to go to next line
+  \t = to add tab
+  \a = to make a sound
+  \\ = to add backslash
+  \' = to add single quote
+  \" = to add double quote
+  
+  */
+
+  cout << "--------------------------------------------------";
+
+  Example4 = "String";
+  Example5 = "Adding";
+
+  cout << "\nExample4 = " << Example4 << endl;
+  cout << "Example5 = " << Example5 << endl;
+
+  cout << "\rExample4\'s contents = " << Example4 << endl;
+  cout << "Example5\"s contents = " << Example5 << endl;
+
+  cout << "\r";
+
+  cout << "\"String\" is in Exaple4" << endl;
+  cout << "\"Adding\" is in Exaple5" << endl;
+
+  cout << "\r\tUsing backslash \\ to add backslash" << endl;
+
+  cout << "\r\aMaking a sound" << endl;
+  
   return 0;
 }
